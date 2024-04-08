60728d8e5b42593e8cf3a42138d5456e81897259
Surya987651234 <suryawanshin2323@gmail.com>
2024-04-08 20:38:51 +0530

    merge conflict test

--- src/main/java/com/demo/StudentDto/Student.java
+++ src/main/java/com/demo/StudentDto/Student.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+// this is git merge conflict test
 @Entity
 @Table(name = "student_db")
 public class Student {
